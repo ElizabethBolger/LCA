790255ec52cc7f761066ab3e943f312e46014a12
Elizabeth Bolger <bolgere3@tcd.ie>
2019-10-13 15:15:06 +0100

    DAG- intialised some variables

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
 
 public class DAG {
-
+	private int V;           // number of vertices in this digraph
+	private int E;                 // number of edges in this digraph
+	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+	private int[] indegree;        // indegree[v] = indegree of vertex v
+	
 }
