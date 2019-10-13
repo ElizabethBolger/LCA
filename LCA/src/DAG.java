c46506fd963c7348d17f0ce8f1c9087a97b0594d
Elizabeth Bolger <bolgere3@tcd.ie>
2019-10-13 15:16:40 +0100

    added constructor and additional variables

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -5,5 +5,20 @@ public class DAG {
 	private int E;                 // number of edges in this digraph
 	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
 	private int[] indegree;        // indegree[v] = indegree of vertex v
+	private boolean marked[];		//Boolean List to track visited vertices
+	private boolean stack[];		//Order that vertices were visited
 	
+	public DAG(int V)
+	{
+		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+	    this.V = V;
+	    this.E = 0;
+	    indegree = new int[V];
+	    marked = new boolean[V];
+	    stack = new boolean[V];
+	    adj = (ArrayList<Integer>[]) new ArrayList[V];
+	    for (int v = 0; v < V; v++) {
+	        adj[v] = new ArrayList<Integer>();
+	    }              
+	}
 }
