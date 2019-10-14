ab0123f6fc4b2671df50f61ad891af18384ea191
Elizabeth Bolger <bolgere3@tcd.ie>
2019-10-14 11:33:15 +0100

    Added DAG test for DG

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -8,5 +8,23 @@ public class DAGTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testForDirectedGraph(){
+		DAG test = new DAG(10);
+		test.addEdge(1, 2);
+		test.addEdge(1, 3);
+		test.addEdge(3, 4);
+		test.addEdge(4, 5);
+		test.addEdge(4, 6);
+
+		assertEquals("", 1, test.indegree(4));
+		assertEquals("", 2, test.outdegree(4));
+		assertEquals("", 5, test.E());
+		assertEquals("", 10, test.V());
+		String ans = "[5, 6]";
+		assertEquals("",ans, test.adj(4).toString());
+	}
+
 
 }
